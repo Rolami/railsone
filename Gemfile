4acb2aa999f505853d9eb8bb42ad64e0783e0d58
Robin Larsson <dreamarcitect@gmail.com>
2019-04-23 22:53:24 +0200

    added new comments for odin

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ ruby '2.6.1'
 gem 'rails', '~> 5.2.3'
 # Use sqlite3 as the database for Active Record
 #gem 'sqlite3'
+#Following odin project here
 group :development, :test do
   gem 'sqlite3' , '~> 1.3.13'
  end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'cars#index'
   resources :cars
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #https://www.theodinproject.com/courses/web-development-101/lessons/your-first-rails-application?ref=lnav
 end
